d550e2d357afa02240cb48783aff64ff9ae9fad7
LeonardvS <schilcherl@gmail.com>
2020-05-02 17:53:43 +0200

    feat: add styling to JobItem component

--- client/src/components/jobItem/JobItem.js
+++ client/src/components/jobItem/JobItem.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './JobItem.css';
 import moment from 'moment';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarker } from '@fortawesome/free-solid-svg-icons'
 
+// TODO: change cursor
 export default function JobItem({ job }) {
+  const [clicked, setClicked] = useState(true);
+
+  function onClickHandler() {
+    return setClicked(!clicked);
+  }
+
   return (
-    <div className="job-item">
-      <div className="job-img-owner">
-        <img className="img" src={job.profilePic} alt={job.name} />
-        <div className="event-owner">{job.eventOwner}</div>
+    <div className="job-item" onClick={onClickHandler}>
+      <div className="job-card">
+        <div className="job-img-owner">
+          <img className="img" src={job.profilePic} alt={job.name} />
+          <div className="event-owner">{job.eventOwner}</div>
+        </div>
+        <div className="job-main-info">
+          <div className="date">{moment(job.startDate).format('Do, MMMM YYYY, h:mm a')}</div>
+          {/* <div>{job.finishDate}</div> */}
+          <div className="title">{job.name.toUpperCase()}</div>
+          <div className="location">
+            <FontAwesomeIcon icon={faMapMarker} />
+            {` ${job.location}`}
+          </div>
+          <div className="job-tags">{job.tags.map((tag) => {
+            return <div className="tag" key={tag}>{tag}</div>
+          })}</div>
+          <div className="job-attendees">{job.attendees.map((attendee) => {
+            return <div className="attendee" key={attendee}>{attendee}</div>
+          })}</div>
+        </div>
       </div>
-      <div className="job-main-info">
-        <div>{moment(job.startDate).format('Do, MMMM YYYY, h:mm a')}</div>
-        {/* <div>{job.finishDate}</div> */}
-        <div>{job.name}</div>
-        <div>{job.location}</div>
-        <div className="job-tags">{job.tags.map((tag) => {
-          return <div key={tag}>{tag}</div>
-        })}</div>
-        <div className="job-attendees">{job.attendees.map((attendee) => {
-          return <div key={attendee}>{attendee}</div>
-        })}</div>
-      </div>
-      {/* <div className="job-description">{job.description}</div> */}
+      {clicked ?
+        <div className="job-extra">
+          <div className="job-description">{job.description}</div>
+          <button className="job-button">
+            I will attend!
+        </button>
+        </div>
+        : null
+      }
     </div>
   );
 }
