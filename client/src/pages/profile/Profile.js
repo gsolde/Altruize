80fe3059b2b6af2696bcfc763c783ae9274ab684
Alejandro Gutierrez <alejandrogutbarce@gmail.com>
2020-05-05 16:11:13 +0200

    chore: Profile page initial setup

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './Profile.css';
+import SignUp from '../signUp/SignUp';
+// import ProfileCard from '../../components/profileCard/ProfileCard';
+import JobList from '../../components/jobList/JobList';
+
+
 
 function Profile () {
   return (
-    <div className="Profile">
-      <h1>Profile !!</h1>
-      <img src="https://media-exp1.licdn.com/dms/image/C4D03AQEuhw7UQwbX5A/profile-displayphoto-shrink_200_200/0?e=1594252800&v=beta&t=CJ7wNArHAR2JQhlbCWOaTUh2i6JjK6YiuR9bQD3GPCo" alt="Gerard...o" />
+    <div className="profile">
+      <div className="card">
+        <SignUp />
+      </div>
+      <div className="dashboard">
+        <h1>Upcoming events</h1>
+        <JobList />
+      </div>
     </div>
   );
 }
