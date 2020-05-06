9e211073ca8a91f726cedcda1d98be2b384a8c11
LeonardvS <schilcherl@gmail.com>
2020-05-06 13:09:48 +0200

    style: add event form finished

--- client/src/pages/addEventPage/AddEventPage.js
+++ client/src/pages/addEventPage/AddEventPage.js
@@ -8,6 +8,98 @@ import { teal, grey } from '@material-ui/core/colors';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+export default function AddEventPage() {
+  const classes = useStyles();
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Container component="main" maxWidth="xs" className={classes.paper}>
+          <React.Fragment>
+            <Typography variant="h6" gutterBottom color="primary">
+              Create Event
+        </Typography>
+            <div className={classes.dateTime}>
+              <KeyboardDatePicker
+                required
+                className={classes.form}
+                disableToolbar
+                variant="inline"
+                format="MM/dd/yyyy"
+                id="date-picker-inline"
+                label="Date"
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+              <KeyboardTimePicker
+                required
+                className={classes.time}
+                id="time-picker"
+                label="Time"
+                KeyboardButtonProps={{
+                  'aria-label': 'change time',
+                }}
+              />
+            </div>
+            <TextField
+              className={classes.form}
+              required
+              id="eventName"
+              name="eventName"
+              label="Event"
+              fullWidth
+              autoComplete="ename"
+            />
+            <TextField
+              className={classes.form}
+              required
+              id="location"
+              name="location"
+              label="Location"
+              fullWidth
+              autoComplete="locname"
+            />
+            <TextField
+              className={classes.form}
+              required
+              id="tags"
+              name="tags"
+              label="Tags"
+              fullWidth
+              autoComplete="tagname"
+            />
+            <TextField
+              required
+              className={classes.description}
+              id="outlined-multiline-static"
+              label="Description"
+              multiline
+              rows={4}
+              variant="outlined"
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Add Event
+          </Button>
+          </React.Fragment>
+        </Container>
+      </MuiPickersUtilsProvider>
+    </MuiThemeProvider>
+  );
+}
 
 const theme = createMuiTheme({
   palette: { primary: teal, secondary: grey },
@@ -15,88 +107,29 @@ const theme = createMuiTheme({
 });
 
 const useStyles = makeStyles((theme) => ({
-  form: {
-    width: '110%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
-  date: {
-    width: '110%', // Fix IE 11 issue.
+  form: {
+    width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   description: {
-    width: '110%', // Fix IE 11 issue.
-    marginTop: theme.spacing(4),
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(5),
   },
   submit: {
-    margin: theme.spacing(2, 0, 6),
+    margin: theme.spacing(4, 0, 6),
   },
+  dateTime: {
+    display: 'flex',
+    marginTop: theme.spacing(3),
+  },
+  time: {
+    marginLeft: theme.spacing(1),
+    marginTop: theme.spacing(1),
+  }
 }));
-
-export default function AddEventPage() {
-  const classes = useStyles();
-
-  return (
-    <MuiThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <React.Fragment>
-          <Typography variant="h6" gutterBottom>
-            Create Event
-        </Typography>
-          <TextField
-            className={classes.form}
-            required
-            id="eventName"
-            name="eventName"
-            label="Event"
-            fullWidth
-            autoComplete="ename"
-          />
-          <TextField
-            className={classes.date}
-            required
-            id="datetime-local"
-            label="Date"
-            type="datetime-local"
-            defaultValue="2020-06-01T12:00"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            fullWidth
-          />
-          <TextField
-            className={classes.form}
-            required
-            id="location"
-            name="location"
-            label="Location"
-            fullWidth
-            autoComplete="locname"
-          />
-          <TextField
-            className={classes.form}
-            required
-            id="tags"
-            name="tags"
-            label="Tags"
-            fullWidth
-            autoComplete="tagname"
-          />
-          <TextareaAutosize
-            className={classes.description}
-            aria-label="minimum height"
-            rowsMin={4}
-            placeholder="Description"
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Add Event
-          </Button>
-        </React.Fragment>
-      </Container>
-    </MuiThemeProvider>
-  );
-}
