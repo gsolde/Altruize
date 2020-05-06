12af878d518b3e0d0b46c6a45c4d741adc636f13
Alejandro Gutierrez <alejandrogutbarce@gmail.com>
2020-05-06 16:14:47 +0200

    style: add SearchIcon to SearchBar

--- client/src/components/searchBar/SearchBar.js
+++ client/src/components/searchBar/SearchBar.js
@@ -1,27 +1,34 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
+import SearchIcon from '@material-ui/icons/Search';
 
-export default function SearchBar() {
+export default function SearchBar () {
   const [search, setSearch] = useState(null);
 
-  function handleSearch({target}) {
+  function handleSearch ({ target }) {
     setSearch(target.value);
   }
 
   // TODO: reset input on submit (might not be working because we currently aren't doing anything with the input)
-  function submitHandler(event) {
+  function submitHandler (event) {
     event.preventDefault();
     setSearch(null);
   }
 
   return (
     <form className="search-wrapper" onSubmit={submitHandler}>
-      <input
-      className="input"
-      type="text"
-      placeholder="Search..."
-      onChange={handleSearch}
-      />
+      <div className="input">
+        <div className="icon">
+          <SearchIcon />
+        </div>
+        <input
+          className="search"
+          type="text"
+          placeholder="Search..."
+          startIcon={<SearchIcon />}
+          onChange={handleSearch}
+        />
+      </div>
     </form>
-  )
+  );
 }
