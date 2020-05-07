d8d694557eb9e0e8c0615d829153d9e7bd4b99b6
LeonardvS <schilcherl@gmail.com>
2020-05-07 17:52:26 +0200

    feat: implement addEvent functionality, events get added to db

--- client/src/services/EventsAPI.js
+++ client/src/services/EventsAPI.js
@@ -1,24 +1,27 @@
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
 
-export async function getAllActiveEvents () {
+export async function getAllActiveEvents() {
   const response = await fetch(`${API_URL}/events/active`);
   return response.json();
 };
 
-export async function getAllPastEvents () {
+export async function getAllPastEvents() {
   const response = await fetch(`${API_URL}/events/past`);
   return response.json();
 };
 
 
-export async function addEvent (event) {
+export async function addEvent(body) {
+  console.log("addEvent -> body", JSON.stringify(body))
   const response = await fetch(`${API_URL}/events`, {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
     },
-    body: JSON.stringify(event),
+    body: JSON.stringify(body),
   });
   return response.json();
 };
+
+//TODO update the relationship between event and org when creating event.
