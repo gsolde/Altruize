343477558a0b4eabc95f0bb18512c08649876fbf
Alejandro Gutierrez <alejandrogutbarce@gmail.com>
2020-05-11 18:22:08 +0200

    feat: Navbar component Avatar show pincture when login

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 import './Nav.css';
 import { Link } from "react-router-dom";
-
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import Avatar from '@material-ui/core/Avatar';
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { makeStyles } from '@material-ui/core/styles';
+import { useSelector } from 'react-redux';
 
-import fakeAuth from '../../FakeAuth';
 
 export default function Nav () {
-
+  const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
+  //TODO change when login is connected
+  const [fakeUser, setFakeUser] = useState({
+    user_name: 'Gerard',
+    profile_pic: 'https://media-exp1.licdn.com/dms/image/C4D03AQEuhw7UQwbX5A/profile-displayphoto-shrink_200_200/0?e=1594252800&v=beta&t=CJ7wNArHAR2JQhlbCWOaTUh2i6JjK6YiuR9bQD3GPCo',
+  });
+  //!
+  const isLoggedIn = useSelector(state => state.isLoggedIn);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -33,7 +41,11 @@ export default function Nav () {
         </Button>
         <div className="log-in-btn">
           <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-            <AccountCircleIcon style={{ color: 'white' }} />
+            {isLoggedIn ?
+              <Avatar alt={fakeUser.user_name} src={`${fakeUser.profile_pic}`} className={classes.small} />
+              :
+              <AccountCircleIcon style={{ color: 'white' }} />
+            }
           </Button>
           <Menu
             id="simple-menu"
@@ -44,7 +56,7 @@ export default function Nav () {
             <MenuItem onClick={handleClose}>
               <Link className="link-accent" to="/profile">Profile</Link>
             </MenuItem>
-            {fakeAuth.isAuthenticated ? (
+            {isLoggedIn ? (
               <MenuItem onClick={handleClose}>
                 <Link className="link-accent" to="/login">Log out</Link>
               </MenuItem>
@@ -63,3 +75,10 @@ export default function Nav () {
     </div>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  }
+}));
