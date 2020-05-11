8e69298f639fb9d72a6f3a820cef3030924003ee
Alejandro Gutierrez <alejandrogutbarce@gmail.com>
2020-05-11 18:37:28 +0200

    chore: Delete FakeAuth file (no need with proper login)

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -3,28 +3,32 @@ import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { isUserLoggedIn, orgId, userId } from '../../actions';
 import LoginForm from '../../components/loginForm/LoginForm';
-import fakeAuth from '../../FakeAuth';
+import { useSelector } from 'react-redux';
 
 
 function Login () {
   const dispatch = useDispatch();
   let history = useHistory();
 <<<<<<< HEAD
+<<<<<<< HEAD
   let location = useLocation();
   let { from } = location.state || { from: { pathname: '/' } };
 =======
 >>>>>>> feat: login page and component working
+=======
+  const isLoggedIn = useSelector(state => state.isLoggedIn);
+>>>>>>> chore: Delete FakeAuth file (no need with proper login)
 
-  return fakeAuth.isAuthenticated ?
+  return isLoggedIn ?
     (
       <div>
         <h1>Logout Page</h1>
         <button
           onClick={() => {
-            fakeAuth.signout(() => history.push("/"));
             dispatch(isUserLoggedIn());
             dispatch(userId(''));
             dispatch(orgId(''));
+            return history.push("/");
           }}
         >
           Log out
