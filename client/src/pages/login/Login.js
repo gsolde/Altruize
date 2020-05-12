9f8ef61412be033dc476c3be61df5e7a04896d53
Alejandro Gutierrez <alejandrogutbarce@gmail.com>
2020-05-12 18:59:41 +0200

    fix: Login page

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -2,20 +2,6 @@ import React from 'react';
 import LoginForm from '../../components/loginForm/LoginForm';
 
 function Login () {
-<<<<<<< HEAD
-  const dispatch = useDispatch();
-  let history = useHistory();
-<<<<<<< HEAD
-<<<<<<< HEAD
-  let location = useLocation();
-  let { from } = location.state || { from: { pathname: '/' } };
-=======
->>>>>>> feat: login page and component working
-=======
-  const isLoggedIn = useSelector(state => state.isLoggedIn);
->>>>>>> chore: Delete FakeAuth file (no need with proper login)
-=======
->>>>>>> feat: FrontEnd: Navbar logout button directly logsout
 
   return (
     <LoginForm />
