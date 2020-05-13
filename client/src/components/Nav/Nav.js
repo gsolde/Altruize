69c90547fe51fa2a43a64bbb99cf81ed8d608366
gsolde <gsolde@gmail.com>
2020-05-13 12:50:00 +0200

    fix: logout redirection

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -8,7 +8,7 @@ import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import { makeStyles } from '@material-ui/core/styles';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useHistory } from "react-router-dom";
-import { isUserLoggedIn, orgId, userId } from '../../actions';
+import { isUserLoggedIn, userInfo, orgInfo, userId, orgId, eventSelectionButton} from '../../actions';
 
 
 export default function Nav () {
@@ -16,15 +16,9 @@ export default function Nav () {
   const dispatch = useDispatch();
   let history = useHistory();
   const [anchorEl, setAnchorEl] = useState(null);
-
-  //TODO change when login is connected
-  const [fakeUser, setFakeUser] = useState({
-    user_name: 'Gerard',
-    profile_pic: 'https://media-exp1.licdn.com/dms/image/C4D03AQEuhw7UQwbX5A/profile-displayphoto-shrink_200_200/0?e=1594252800&v=beta&t=CJ7wNArHAR2JQhlbCWOaTUh2i6JjK6YiuR9bQD3GPCo',
-  });
-  //!
-
   const isLoggedIn = useSelector(state => state.isLoggedIn);
+  const userPic = useSelector(state => state.userInfo.profile_pic);
+  const userName = useSelector(state => state.userInfo.user_name);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -37,6 +31,9 @@ export default function Nav () {
     dispatch(isUserLoggedIn());
     dispatch(userId(''));
     dispatch(orgId(''));
+    dispatch(userInfo({}));
+    dispatch(orgInfo({}));
+    dispatch(eventSelectionButton('ALL EVENTS'));
     localStorage.removeItem("altruize-token");
     return history.push("/");
   };
@@ -54,7 +51,7 @@ export default function Nav () {
         <div className="log-in-btn">
           <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
             {isLoggedIn ?
-              <Avatar alt={fakeUser.user_name} src={`${fakeUser.profile_pic}`} className={classes.small} />
+              <Avatar alt={userName} src={`${userPic}`} className={classes.small} />
               :
               <AccountCircleIcon style={{ color: 'white' }} />
             }
