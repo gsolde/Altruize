966d8f47fdc365c8700e85ba1471bbc41ec67ecd
gsolde <gsolde@gmail.com>
2020-05-14 11:43:51 +0200

    style: menu style

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -60,20 +60,20 @@ export default function Nav () {
             onClose={handleClose}
           >
             <MenuItem onClick={handleClose}>
-              <Link className="link-accent" to="/profile">Profile</Link>
+              <Link style={{fontSize: '13px'}} className="link-accent" to="/profile">Profile</Link>
             </MenuItem>
             <MenuItem onClick={handleClose}>
-              <Link className="link-accent" to="/about">About</Link>
+              <Link style={{fontSize: '13px'}} className="link-accent" to="/about">About</Link>
             </MenuItem>
             {isLoggedIn ? (
               <MenuItem onClick={handleClose}>
-                <div onClick={handleLogOut}>
+                <div style={{fontSize: '13px'}} onClick={handleLogOut}>
                   Log out
                 </div>
               </MenuItem>
             ) : (
                 <MenuItem onClick={handleClose}>
-                  <Link className="link-accent" to="/login">Log in</Link>
+                  <Link style={{fontSize: '13px'}} className="link-accent" to="/login">Log in</Link>
                 </MenuItem>
               )
             }
